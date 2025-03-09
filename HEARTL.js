5b6bd76f42c5c0e469ee058b6b0d6ac93351661c
AndresX-maker <andrestrompac@yahoo.com>
2025-03-09 20:07:49 +0100

    Update HEARTL.js
    Signed-off-by: AndresX-maker <andrestrompac@yahoo.com>

--- HEARTL.js
+++ HEARTL.js
@@ -1,119 +1,118 @@
+// Ottieni l'altezza dell'header
+var header = document.getElementById('header');
+var headerHeight = header ? header.offsetHeight : 0;
+
+// Imposta le dimensioni del canvas tenendo conto dell'altezza dell'header
+O = c.width = innerWidth;
+Q = c.height = innerHeight - headerHeight;
+
 e = [];// trails
 h = [];// heart path
-O = c.width = innerWidth;
-Q = c.height = innerHeight;
 
 v = 32; // num trails, num particles per trail & num nodes in heart path
 M = Math;
 R = M.random;
 C = M.cos;
-Y = 6.3;// close to 44/7 or Math.PI * 2 - 6.3 seems is close enough. 
-for( i = 0; i <Y; i+= .2 ) { // calculate heart nodes, from http://mathworld.wolfram.com/HeartCurve.html
-	h.push([
-		O/2 + 180*M.pow(M.sin(i), 3),
-		Q/2 + 10 * (-(15*C(i) - 5*C(2*i) - 2*C(3*i) - C(4*i)))
-	])
+Y = 6.3; // approssimativamente 2*PI
+
+// Calcola i nodi della curva del cuore (fonte: http://mathworld.wolfram.com/HeartCurve.html)
+for(i = 0; i < Y; i += 0.2) {
+  h.push([
+    O/2 + 180 * M.pow(M.sin(i), 3),
+    Q/2 + 10 * ( - (15 * C(i) - 5 * C(2*i) - 2 * C(3*i) - C(4*i)) )
+  ]);
 }
 
 i = 0;
-while (i < v ) {
+while (i < v) {
 
-	x = R() * O;
-	y = R() * Q;
-	//r = R() * 50 + 200;
-	//b = R() * r;
-	//g = R() * b;
+  x = R() * O;
+  y = R() * Q;
 
-	H = i/v * 80 + 280;
-	S = R() * 40 + 60;
-	B = R() * 60 + 20;
+  H = i/v * 80 + 280;
+  S = R() * 40 + 60;
+  B = R() * 60 + 20;
 
-	f = []; // create new trail
+  f = []; // crea una nuova traccia
 
-	k = 0;
-	while ( k < v ) { 
-		f[k++] = { // create new particle
-			x : x, // position 
-			y : y,
-			X : 0, // velocity
-			Y : 0,
-			R : (1 - k/v)  + 1, // radius
-			S : R() + 1, // acceleration 
-			q : ~~(R() * v), // target node on heart path
-			//D : R()>.5?1:-1,
-			D : i%2*2-1, // direction around heart path
-			F : R() * .2 + .7, // friction
-			//f : "rgba(" + ~~r + "," + ~~g + "," + ~~b + ",.1)"
-			f : "hsla("+~~H+","+~~S+"%,"+~~B+"%,.1)" // colour
+  k = 0;
+  while (k < v) {
+    f[k++] = { // crea una nuova particella
+      x: x, // posizione
+      y: y,
+      X: 0, // velocità orizzontale
+      Y: 0, // velocità verticale
+      R: (1 - k/v) + 1, // raggio
+      S: R() + 1, // accelerazione
+      q: ~~(R() * v), // nodo target sulla curva del cuore
+      D: i % 2 * 2 - 1, // direzione lungo il percorso del cuore
+      F: R() * 0.2 + 0.7, // attrito
+      f: "hsla(" + ~~H + "," + ~~S + "%," + ~~B + "%,.1)" // colore
+    }
+  }
+  
+  e[i++] = f; // e contiene le tracce (ogni traccia è un array di particelle)
+}
+
+function render(p) { // disegna una particella
+  a.fillStyle = p.f;
+  a.beginPath();
+  a.arc(p.x, p.y, p.R, 0, Y, 1);
+  a.closePath();
+  a.fill();
+}
+
+function loop() {
+
+  a.fillStyle = "rgba(0,0,0,.2)"; // cancella lo schermo
+  a.fillRect(0, 0, O, Q);
+
+  i = v;
+  while (i--) {
+
+    f = e[i]; // seleziona la traccia corrente
+    u = f[0]; // prima particella della traccia
+    q = h[u.q]; // nodo target sulla curva del cuore
+    D = u.x - q[0]; // distanza orizzontale dal nodo
+    E = u.y - q[1]; // distanza verticale dal nodo
+    G = M.sqrt(D * D + E * E);
+    
+    if (G < 10) { // la traccia ha raggiunto il nodo target?
+      if (R() > 0.95) { // invia casualmente la traccia in un altro nodo
+        u.q = ~~(R() * v);
+      } else {
+        if (R() > 0.99) u.D *= -1; // cambio casuale di direzione
+        u.q += u.D;
+        u.q %= v;
+        if (u.q < 0) u.q += v;
+      }
+    }
+    
+    // Calcola e applica la velocità
+    u.X += -D / G * u.S;
+    u.Y += -E / G * u.S;
+    u.x += u.X;
+    u.y += u.Y;
+    
+    render(u); // disegna la prima particella
+    u.X *= u.F; // applica attrito
+    u.Y *= u.F;
+    
+    k = 0;
+    while (k < v - 1) { // ciclo sulle particelle rimanenti della traccia
       
-		}
-	}
+      T = f[k];      // particella corrente
+      N = f[++k];    // particella successiva
+      
+      N.x -= (N.x - T.x) * 0.7; // effetto "paradosso di Zenone" per creare la scia
+      N.y -= (N.y - T.y) * 0.7;
+      
+      render(N);
+    }
+  }
+} // fine loop()
 
-	e[i++] = f; // dots are a 2d array of trails x particles
-}
-
-function render(_) { // draw particle
-	a.fillStyle = _.f;
-	a.beginPath();
-	a.arc(_.x, _.y, _.R, 0, Y, 1);
-	a.closePath();
-	a.fill();
-}
-
-function loop(){
-
-	a.fillStyle = "rgba(0,0,0,.2)"; // clear screen
-	a.fillRect(0,0,O,Q);
-
-	i = v;
-	while (i--) {
-
-		f = e[ i ]; // get worm
-		u = f[ 0 ]; // get 1st particle of worm
-		q = h[ u.q ]; // get current node on heart path
-		D = u.x - q[0]; // calc distance
-		E = u.y - q[1];
-		G = M.sqrt( (D * D) + (E * E) );
-		
-		if ( G < 10 ) { // has trail reached target node?
-			if (R() > .95 ) { // randomly send a trail elsewhere
-				u.q = ~~(R() * v);
-			} else {
-				if ( R() > .99) u.D *= -1; // randomly change direction
-				u.q += u.D;
-				u.q %= v;
-				if ( u.q < 0 ) u.q += v;
-			 }
-		}
-
-		u.X += -D / G * u.S; // calculate velocity
-		u.Y += -E / G * u.S;
-
-		u.x += u.X; // apply velocity
-		u.y += u.Y;
-
-		render(u); // draw the first particle
-
-		u.X *= u.F; // apply friction
-		u.Y *= u.F;
-
-		k = 0;
-		while ( k < v-1 ) { // loop through remaining dots
-			
-			T = f[ k ]; // this particle
-			N = f[ ++k ]; // next particle
-
-			N.x -= (N.x - T.x) * .7; // use zenos paradox to create trail
-			N.y -= (N.y - T.y) * .7;
-
-			render(N);
-
-		}
-
-	}
-}; // eo loop()
-
-(function doit(){
-	requestAnimationFrame(doit);
-	loop();
+(function animate() {
+  requestAnimationFrame(animate);
+  loop();
 }());
